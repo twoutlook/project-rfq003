d0d6633745348f0455514bb354080d51912e9acb
Mark <twcloudwebapp@outlook.com>
2017-03-29 11:59:32 +0800

    修改压制小计 计算公式

--- B0329/rfq/js/twcloud/index-excel/patch-A0601.js
+++ B0329/rfq/js/twcloud/index-excel/patch-A0601.js
@@ -133,6 +133,9 @@ function getPatchCellsA0601_3() {
     return cells;
 }
 
+
+// 2-9) 难易系数
+// 计算公式為：旧版压铸費總额* “难易系数” = 新版的压铸費
 function getPatchCellsA0601_5() {
     var cells = [];
     cells.push(
@@ -155,6 +158,21 @@ function getPatchCellsA0601_5() {
 
             {sheet: 1, row: 49, col: 1, json: {data: "XXX"}},//for debug
             {sheet: 1, row: 49, col: 2, json: {data: "難度係數 "}},
+            {sheet: 1, row: 49, col: 3, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            {sheet: 1, row: 49, col: 4, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            {sheet: 1, row: 49, col: 5, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            {sheet: 1, row: 49, col: 6, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            {sheet: 1, row: 49, col: 7, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            {sheet: 1, row: 49, col: 8, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
+            //影響到的計算小計
+            // {sheet: 1, row: 50, col: 3, json: styleSubTotal({data: setNaToZero("C49*(C44+C45)*(1+(1-C47/100))/C16")})},
+            {sheet: 1, row: 50, col: 3, json: styleSubTotal({data: setNaToZero("C49*(C44+C45)*(1+(1-C47/100))/C16")})},
+{sheet: 1, row: 50, col: 4, json: styleSubTotal({data: setNaToZero("D49*(D44+D45)*(1+(1-D47/100))/D16")})},
+{sheet: 1, row: 50, col: 5, json: styleSubTotal({data: setNaToZero("E49*(E44+E45)*(1+(1-E47/100))/E16")})},
+{sheet: 1, row: 50, col: 6, json: styleSubTotal({data: setNaToZero("F49*(F44+F45)*(1+(1-F47/100))/F16")})},
+{sheet: 1, row: 50, col: 7, json: styleSubTotal({data: setNaToZero("G49*(G44+G45)*(1+(1-G47/100))/G16")})},
+{sheet: 1, row: 50, col: 8, json: styleSubTotal({data: setNaToZero("H49*(H44+H45)*(1+(1-H47/100))/H16")})},
+//       
             // {sheet: 1, row: 82, col: 3, json: styleInput({fm: "number", dfm: "0.0", data: "1"})},
          
 
